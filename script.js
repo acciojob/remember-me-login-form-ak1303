67eae0e46366d9768a0cd1aa7782398e897e258d
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-03-21 23:41:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+	let submitBtn = document.getElementById('submit');
+	let checkbox = document.getElementById('checkbox');
+	let username=document.getElementById('username');
+	let password=document.getElementById('password');
+	let form = document.getElementsByTagName('form')[0];
+	submitBtn.addEventListener('click',(e)=>{
+		if(checkbox.checked){
+			localStorage.setItem('username',username.value);
+			localStorage.setItem('password',password.value);
+		}else{
+			localStorage.removeItem('username');
+			localStorage.removeItem('password');
+		}
+		alert(`Logged in as ${username.value}`)
+	})
+	
+	let user = localStorage.getItem('username') 
+	if(user){
+		let loginBtn = document.createElement('button');
+		loginBtn.id = 'existing';
+		loginBtn.innerText = 'Login as existing user';
+		form.append(loginBtn);
+		loginBtn.addEventListener('click',()=>{
+			alert(`Logged in as ${user}`);
+		})
+	}
+});
+
+
+
+
